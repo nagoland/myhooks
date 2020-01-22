03c9ee4a66d58d9f2dd371cf3fbd11bdd8c866bb
nago_land <www1224a@gmail.com>
2020-01-22 18:16:06 +0900

    added reducer

--- src/App.js
+++ src/App.js
@@ -1,40 +1,63 @@
-import React,{useState} from "react"
+import React,{useEffect,useState} from "react"
 import User from "./User"
 
-const App = () => {
-    const [text, setText] = useState("入力してね")
-    const [names, setNames] = useState(["nagoya","wataru"])
-    const [input, setInput] = useState("")
-    const changeInput = (e) => {
+const App = (props) => {
+    // const [text, setText] = useState("入力してね")
+    // const [names, setNames] = useState(props.names)
+    // const [input, setInput] = useState(props.input)
+    const [state, setState] = useState(props)
+    const [activeUser, setActiveUser] = useState([state.names[0]])
 
-        setInput(e.target.value)
+
+
+    const changeInput = (e) => {
+        setState({...state,input:e.target.value})
     }
     const addName = (e) => {
         e.preventDefault()
-        if(input === ""){
-            return(
-                setText("カラですよー")
-            )
+        if(state.input === ""){
+            // return(
+            //     setText("カラですよー")
+            // )
         }else{
-            setText(input + "さんが仲間になりました")
-            setNames(names.concat(input))
-            setInput("")
+            setState({...state,
+                names: state.names.concat(state.input),
+                input: ""})
+            console.log(state.names)
         }
+    }
+    // useEffect(()=>{
+    //     setState({...state,input:""})
+    // })
+
+    const changeActiveUser= (e) => {
+        e.preventDefault()
+        const activeIndex = Math.floor(Math.random()*(state.names.length))
+        setActiveUser(state.names[activeIndex])
     }
     return (
         <>
         <form>
-            <input type="text"　onChange={changeInput} value={input}></input>
+            <input type="text"　onChange={changeInput} value={state.input}></input>
             <button onClick={addName}>追加</button>
+            <button onClick={changeActiveUser}>チェンジ！</button>
         </form>
-        <h1>{text}</h1>
+        {/* <h1>{text}</h1> */}
         {
-            names.map((name,index)=>(
+            state.names.map((name,index)=>(
                 <User name={name} key={index}/>
             ))
-        } 
+        }
+        <h1>{activeUser}</h1>
         </>
     )
 }
+// 
+App.defaultProps =  {
+    names: ["nagoya","wataru"],
+    input: ""
+}
+
 
 export default App
+
---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+
+
+const events = (state=[], action) => {
+
+    switch(action.type){
+        case "SEND_MESSAGE":
+            return state
+        case "DELETE_MESSAGE":
+            return state
+        case "DELETE_ALL_MESSAGE"
+        default:
+            return state
+    }
+
+}
+
+export default events
